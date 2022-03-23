8c9975294edad9fa9488b5a14a951f50904888b8
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2022-03-22 22:54:15 -0700

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const MONGODB_URI = "mongodb+srv://peterguo:pgdEbfRDEPFzpsFD@cluster0.objtj.mongodb.net/redux-store?retryWrites=true&w=majority";
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/redux-store', {
   useNewUrlParser: true,
